2b93cb77b672af8d5561fd854920b94f3bf090f0
gustavoparaiso <gustavoparaiso@proton.me>
2026-01-16 23:16:05 +0000

    setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,6 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Stack } from "expo-router";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+const RootLayout = () => <Stack screenOptions={{ headerShown: false }} />;
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+export default RootLayout;
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
-}
